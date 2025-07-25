a01eba301d3b5b73a12783f71594ac08248767e3
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-25 12:07:39 +0530

    add HTTP request examples for testing Axum server in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello World From Axum With Rust! 🦀" }));
+
+    // run our app with hyper, listening globally on port 5000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+    let app_res= axum::serve(listener, app).await;
+    match app_res {
+        Ok(_) => println!("Server running on http://localhost:5000"),
+        Err(e) => eprintln!("Server error: {}", e),
+    }
 }
